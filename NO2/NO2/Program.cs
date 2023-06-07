8690b790da2f59636847e02c414e575aa1b7b38c
sairemgloria <sairemgloria1999@gmail.com>
2023-06-07 16:32:02 +0800

    Tutorial 2

---
+++ NO2/NO2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnum = 10;
+            int secondnum = 3;
+            string firstname = "sairem";
+            string lastname = "gloria";
+
+            Console.WriteLine("My fullname is " + firstname + " " + lastname); // printing the name of the user
+        }
+    }
+}
